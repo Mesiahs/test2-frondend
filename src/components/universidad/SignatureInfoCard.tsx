edafd3ab251745e7ad3ed3502844451a655307ad
Mesiahs <sebastianmesias04@gmail.com>
2025-08-07 17:00:40 -0500

    cambios para el aparetado de sobre carrera\

--- src/components/universidad/SignatureInfoCard.tsx
+++ src/components/universidad/SignatureInfoCard.tsx
@@ -3,21 +3,32 @@ import React from 'react'
 
 const SignatureInfoCard = ({ onClose, signature }: { onClose: Function, signature: Carrera | null | undefined }) => {
   const materias = signature?.planEstudios
+  const sobreCarreraInfo = signature?.informacion_carrera.Sobre_carrera
+  const showInfoCarrera = () => {
+    if(sobreCarreraInfo && sobreCarreraInfo?.length > 0) {
+
+      return sobreCarreraInfo.map(info => <p className="text-gray-700 mt-2">
+          {info}
+        </p> )
+    }
+    return <p className="text-gray-700 mt-2">
+          {signature?.informacion_carrera.Sobre_carrera}
+        </p>
+  }
   return (
     <div
       className="fixed inset-0 flex items-center justify-center bg-[#ffffffac] backdrop-blur-xs bg-opacity-50 "
       onClick={() => onClose()}
     >
       <div
-        className="bg-white max-h-4/5 mt-10 backdrop-blur-3xl w-3/4 p-6 rounded-lg shadow-lg flex flex-col items-center"
+        className="bg-white max-h-4/5 mt-10 backdrop-blur-3xl w-3/4 p-6 rounded-lg shadow-lg flex flex-col items-center overflow-y-scroll "
         onClick={(e) => e.stopPropagation()}
       >
         <h3 className="text-lg font-bold text-orange-800">Bienvenido, a {signature?.nombre_carrera}</h3>
-        <p className="text-gray-700 mt-2">
-          {signature?.informacion_carrera.Sobre_carrera}
-        </p>
+        
+        {showInfoCarrera()}
 
-        <section className='flex overflow-x-scroll w-full mt-4 '>
+        <section className='flex overflow-x-scroll overflow-y-scroll w-full mt-4 min-h-96 '>
           {materias?.map((materia, index) =>
             <section key={index} className='min-w-52 bg-gradient-to-b from-amber-100 to-white rounded-xl  ' >
               <h4 className='text-center pb-3 font-semibold' >Semestre {materia.numeroSemestre}</h4>
